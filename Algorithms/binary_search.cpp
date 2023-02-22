3e1c1392c12a872eaafc919bbc8675f926da3646
ChazGrant <ilya20012209@yandex.ru>
2023-02-22 21:33:06 +0300

    Implemented Binary Search

--- Algorithms/binary_search.cpp
+++ Algorithms/binary_search.cpp
@@ -1,9 +1,28 @@
+#include <iostream>
 #define ARRAY_SIZE 5
 
 
-int binary_search(int arr[ARRAY_SIZE]);
+int binarySearch(int arr[ARRAY_SIZE], int value, int left, int right);
 
 int main()
 {
+    int arr[ARRAY_SIZE] = {2, 3, 5, 7, 9};
+    std::cout << binarySearch(arr, 4, 0, ARRAY_SIZE - 1);
     return 0;
+}
+
+int binarySearch(int arr[ARRAY_SIZE], int value, int left, int right)
+{
+    if (left > right)
+        return -1;
+
+    int middle = (left + right) / 2;
+    if (arr[middle] < value)
+        left = middle + 1;
+    else if(arr[middle] > value)
+        right = middle - 1;
+    else
+        return middle;
+
+    return binarySearch(arr, value, left, right);
 }
