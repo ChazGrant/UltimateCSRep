d65407eeac766bdf389a2b352eb4f09f20290e99
ChazGrant <ilya20012209@yandex.ru>
2023-02-27 00:56:49 +0300

    Temporary implementation of merge sorting

---
+++ Algorithms/merge_sort.cpp
@@ -0,0 +1,68 @@
+#define ARRAY_SIZE 5
+#include <iostream>
+
+void merge(int *, int, int, int);
+void mergeSort(int *, int, int);
+
+int main()
+{
+    int arr[ARRAY_SIZE] = {21, -5, -9, 2, 3};
+    mergeSort(arr, 0, ARRAY_SIZE - 1);
+
+    for (auto &el:arr)
+        std::cout << el << " ";
+    std::cout << std::endl;
+    return 0;
+}
+
+void merge(int *arr, int low, int high, int mid)
+{
+    int i, j, k, c[50];
+    i = low;
+    k = low;
+    j = mid + 1;
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            c[k] = arr[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            c[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        c[k] = arr[i];
+        k++;
+        i++;
+    }
+
+    while (j <= high)
+    {
+        c[k] = arr[j];
+        k++;
+        j++;
+    }
+
+    for (i = low; i < k; ++i)
+        arr[i] = c[i];
+}
+
+void mergeSort(int *arr, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, high, mid);
+    }
+}
